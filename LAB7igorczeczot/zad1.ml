c102c2f7bda4664e804398f4abd1707cb1b56f0e
igorooo <igorczeczot@windowslive.com>
2018-11-30 15:24:33 +0100

    lista 7 zad 1, zad2

--- LAB7igorczeczot/zad1.ml
+++ LAB7igorczeczot/zad1.ml
@@ -1,30 +1,30 @@
-type 'a llist= LNil| LCons of 'a * (unit -> 'a llist);;
 
 type eval = Num of float | Neg | Add | Sub | Mul | Div| Neutral;;
 
 exception Ex of string;;
 
-let rec funct stack elemA elemB=
-  match (List.hd stack) with
-  |[] -> elemA
-  |Num a -> if(elemA <> Neutral) then funct (List.tl stack) (List.hd stack) Neutral
-          else ( if elemB <> Neutral then funct (List.tl stack) elemA (List.hd stack)
-                 else raise(Ex "wrong operation!"))
-  |Neg -> if(elemA <> Neutral) 
-            then let eff = elemA *. (-1.0) in funct (List.tl stack) eff Neutral
-          else raise(Ex "wrong operation!")
-  |Add -> if(elemA <> Neutral && elemB <> Neutral) 
-            then let eff = elemA +. elemB in funct (List.tl stack) eff Neutral
-          else raise(Ex "wrong operation!")
-  |Sub -> if(elemA <> Neutral && elemB <> Neutral) 
-            then let eff = elemA -. elemB in funct (List.tl stack) eff Neutral
-          else raise(Ex "wrong operation!")
-  |Mul -> if(elemA <> Neutral && elemB <> Neutral) 
-            then let eff = elemA *. elemB in funct (List.tl stack) eff Neutral
-          else raise(Ex "wrong operation!")
-  |Div -> if(elemA <> Neutral && elemB <> Neutral) 
-            then let eff = elemA /. elemB in funct (List.tl stack) eff Neutral
-          else raise(Ex "wrong operation!")
+let stackMach lista =
+  let rec funct list elA elB flag = if list = [] then elA else
+    let h::t = list
+    in match flag with
+     | 0 -> (match h with
+            | Neg -> funct t (elA *. (-1.)) elB 0
+            | Num a -> funct t elA a 1
+            | _ -> raise(Ex "wrong operation!") )
+     | 1 -> (match h with
+           | Add -> funct t (elA +. elB) 0. 0
+           | Sub -> funct t (elA -. elB) 0. 0
+           | Mul -> funct t (elA *. elB) 0. 0
+           | Div -> funct t (elA /. elB) 0. 0
+           | _ -> raise(Ex "wrong operation!") )
+  in if lista = [] then 0. else let h::t = lista in match h with 
+                                                    | Num a -> funct t a 0. 0
+                                                    | _ -> raise(Ex "wrong operation!")
 ;;
 
-  
+let x = stackMach [Num 5.;Num 3.; Add;];;
+
+let xx = stackMach [Num 5.;Num 3.; Add;Neg;Num 10.; Add;Num 5.; Mul; Num 4.; Div];;
+
+let xxx = stackMach [Num 5.;Num 3.; Add;Neg;Num 10.; Add;Num 5.; Mul; Num 4.; Div; Sub];;
+            
--- LAB7igorczeczot/zad2.ml
+++ LAB7igorczeczot/zad2.ml
@@ -1,4 +1,5 @@
 type 'a llist= LNil| LCons of 'a * (unit -> 'a llist);;
+
 let rec lfrom k = LCons(k, function () -> lfrom(k+1));;
 
 let rec ltake = function
@@ -7,20 +8,22 @@ let rec ltake = function
   |(n, LCons(x,xf)) -> x::ltake(n-1,xf())
 ;;
 
-let rec lmulti list cur=
-  let rec multiE elem am = if am > 0 then LCons(elem, function() -> multiE elem (am-1)) else LNil
-  in match list with
-    |LNil -> LNil
-    |LCons(h,t) -> if h <> cur then LCons(multiE h h, function() -> lmulti (t()) h) else lmulti(t()) h
+(*FUNCKJA WLASCIWA*)
+
+let lMult lista = 
+  let rec lmulti list cur=
+    match list with
+      |LNil -> LNil
+      |LCons(h,t) -> if cur > 0 then LCons(h, function() -> lmulti list (cur-1)) 
+                     else match t() with
+                      |LNil -> LNil
+                      |LCons(hd,tl) -> lmulti (t()) hd
+  in lmulti lista 0
 ;;
 
-let listA = lfrom 1;;
 
-let listB = lmulti listA 0;;
+let listA = lfrom 0;;
 
-ltake(10, listB);;
+let listB = lMult listA;;
 
-
-let rec multiE elem am = if am > 0 then LCons(elem,fun() -> multiE elem (am-1)) else LNil;;
-
-ltake(10, multiE 1 5);;
+ltake(16, listB);;
